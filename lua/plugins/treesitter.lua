11b5ef4cd6aa666c4511ed4d931b7d0cc53313c2
Dikshit singh <singhdikshit69@gmail.com>
2023-12-21 17:31:02 +0530

    chore(treesitter.lua): format code for better readability feat(treesitter.lua): add nvim-ts-autotag plugin to automatically close HTML tags and improve editing experience

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {{
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua", "javascript","bash","html","css","python","json"},
+            ensure_installed = {"lua", "javascript", "bash", "html", "css", "python", "json"},
             highlight = {
                 enable = true
             },
@@ -14,6 +14,15 @@ return {{
             autotag = {
                 enable = true
             }
+        })
+    end
+}, {
+    "windwp/nvim-ts-autotag",
+    config = function()
+        local config = require("nvim-ts-autotag")
+        config.setup({
+            filetypes = {'html', 'xml', 'javascript','typescript','jsx','tsx','typescript','markdown'},
+            enable_close_on_exit = true
         })
     end
 }}
