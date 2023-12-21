5b789d0e1f37ff135c75815bc5d450b3417467a5
Dikshit singh <singhdikshit69@gmail.com>
2023-12-21 13:16:42 +0530

    Add lsp support for python, web and bash

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,7 +7,7 @@ return {{
     "williamboman/mason-lspconfig.nvim",
     config = function()
         require("mason-lspconfig").setup({
-            ensure_installed = {"lua_ls", "tsserver"}
+            ensure_installed = {"lua_ls", "tsserver", "pyright", "html", "jsonls", "cssls", "bashls"}
         })
     end
 }, {
@@ -16,6 +16,11 @@ return {{
         local lspconfig = require("lspconfig")
         lspconfig.lua_ls.setup({})
         lspconfig.tsserver.setup({})
+        lspconfig.pyright.setup({})
+        lspconfig.html.setup({})
+        lspconfig.jsonls.setup({})
+        lspconfig.cssls.setup({})
+        lspconfig.bashls.setup({})
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
         vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
