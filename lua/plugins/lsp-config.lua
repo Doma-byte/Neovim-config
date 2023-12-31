92b1bca5c9f448431f3da6b026fe753ea5f189d5
Dikshit singh <singhdikshit69@gmail.com>
2023-12-31 10:58:59 +0530

    chore(lsp-config.lua): reformat and optimize lsp configuration
    The changes in this commit reformat and optimize the configuration for the language server protocol (LSP) in the `lsp-config.lua` file. The following changes were made:
    
    - Reformat the configuration to improve readability and maintainability.
    - Add the `lazy` option to each plugin to improve performance by only loading the plugin when needed.
    - Add the `opts` option to the `mason-lspconfig.nvim` plugin to enable auto installation of required language servers.
    - Add the `capabilities` variable to the `nvim-lspconfig` plugin to set default capabilities for the language servers.
    - Remove unnecessary setup calls for language servers that are already included in the `mason-lspconfig.nvim` plugin.
    - Update key mappings for hover, definition, references, and code action.
    
    These changes aim to improve the overall configuration and performance of the language server integration in the project.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,39 @@
-return {{
-    "williamboman/mason.nvim",
-    config = function()
+return {
+    {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
         require("mason").setup()
-    end
-}, {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {"lua_ls", "tsserver", "pyright", "html", "jsonls", "cssls", "bashls"}
-        })
-    end
-}, {
-    "neovim/nvim-lspconfig",
-    config = function()
+      end,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      opts = {
+        auto_install = true,
+      },
+    },
+    {
+      "neovim/nvim-lspconfig",
+      lazy = false,
+      config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  
         local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({})
-        lspconfig.tsserver.setup({})
-        lspconfig.pyright.setup({})
-        lspconfig.html.setup({})
-        lspconfig.jsonls.setup({})
-        lspconfig.cssls.setup({})
-        lspconfig.bashls.setup({})
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-}}
+        lspconfig.tsserver.setup({
+          capabilites = capabilities,
+        })
+        lspconfig.html.setup({
+          capabilites = capabilities,
+        })
+        lspconfig.lua_ls.setup({
+          capabilites = capabilities,
+        })
+  
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      end,
+    },
+  }
