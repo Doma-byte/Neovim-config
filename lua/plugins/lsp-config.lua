84e87c3c1c70e437cae689c06ea274f5e0b96174
Dikshit singh <singhdikshit69@gmail.com>
2024-01-06 20:20:51 +0530

    chore(lsp-config.lua): reformat code for better readability and add key mappings for vertical split and Mason command

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,39 +1,47 @@
 return {
-    {
-      "williamboman/mason.nvim",
-      lazy = false,
-      config = function()
-        require("mason").setup()
-      end,
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      lazy = false,
-      opts = {
-        auto_install = true,
-      },
-    },
-    {
-      "neovim/nvim-lspconfig",
-      lazy = false,
-      config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  
-        local lspconfig = require("lspconfig")
-        lspconfig.tsserver.setup({
-          capabilites = capabilities,
-        })
-        lspconfig.html.setup({
-          capabilites = capabilities,
-        })
-        lspconfig.lua_ls.setup({
-          capabilites = capabilities,
-        })
-  
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      end,
-    },
-  }
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
+				capabilites = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilites = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilites = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<C-t>", ":vsplit", {
+				noremap = true,
+				silent = true,
+			})
+			vim.keymap.set("n", "<C-m>", ":Mason", {
+				noremap = true,
+				silent = true,
+			})
+		end,
+	},
+}
