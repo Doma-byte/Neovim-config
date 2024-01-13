eafe265ea2811fb4b6ad83d41a15bf844b6cef5d
Dikshit singh <singhdikshit69@gmail.com>
2024-01-13 19:06:17 +0530

    chore(alpha.lua): fix formatting and add newline at end of file chore(auto-pairs.lua): fix formatting chore(catppuccin.lua): fix formatting chore(comments.lua): fix formatting

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,13 +1,15 @@
-return {{
-    "tpope/vim-commentary",
-    config = function()
-        vim.keymap.set('n', 'gcc', ':Commentary<CR>', {
-            noremap = true,
-            silent = true
-        })
-        vim.keymap.set('x', 'gc', ':Commentary<CR>', {
-            noremap = true,
-            silent = true
-        })
-    end
-}}
+return {
+	{
+		"tpope/vim-commentary",
+		config = function()
+			vim.keymap.set("n", "gcc", ":Commentary<CR>", {
+				noremap = true,
+				silent = true,
+			})
+			vim.keymap.set("x", "gc", ":Commentary<CR>", {
+				noremap = true,
+				silent = true,
+			})
+		end,
+	},
+}
