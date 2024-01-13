a053f53396d74b77c3fc864df458dea22acbc85a
Dikshit singh <singhdikshit69@gmail.com>
2024-01-13 19:06:58 +0530

    feat(plugins): update neo-tree.lua, none-ls.lua, nvim-tree.lua, tagbar.lua, telescope.lua, treesitter.lua
    - Update neo-tree.lua:
      - Change indentation and formatting for better readability.
    - Update none-ls.lua:
      - Change indentation and formatting for better readability.
    - Update nvim-tree.lua:
      - Change indentation and formatting for better readability.
    - Update tagbar.lua:
      - Change indentation and formatting for better readability.
    - Update telescope.lua:
      - Change indentation and formatting for better readability.
    - Update treesitter.lua:
      - Change indentation and formatting for better readability.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,31 @@
-return {{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            auto_install = true,
-            highlight = {
-                enable = true
-            },
-            indent = {
-                enable = true
-            },
-            autotag = {
-                enable = true
-            }
-        })
-    end
-}, {
-    "windwp/nvim-ts-autotag",
-    config = function()
-        local config = require("nvim-ts-autotag")
-        config.setup({
-            filetypes = {'html', 'xml', 'javascript','typescript','jsx','tsx','markdown'},
-            enable_close_on_exit = true
-        })
-    end
-}}
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+				autotag = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			local config = require("nvim-ts-autotag")
+			config.setup({
+				filetypes = { "html", "xml", "javascript", "typescript", "jsx", "tsx", "markdown" },
+				enable_close_on_exit = true,
+			})
+		end,
+	},
+}
